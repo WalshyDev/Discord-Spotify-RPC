8a4cfac6dd754cc9d4acfc8f367a6b00f9e53c82
WalshyDev <bwfcwalshy@gmail.com>
2017-11-27 10:38:18 +0000

    Initial push

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Discord-Spotify-RPC
-Spotify Rich Presence for Discord made in NodeJS
+# Install
+Download the zip, extract to a directory then open CMD in that directory and run `npm i` and let npm install the packages.
+
+# Setup
+Sadly some libs haven't been updated in a while to apply some fixes so you will need to edit 2 files in order for this to work.
+
+## wintools
+Edit the /lib/ps.js so line 11 has a maxBuffer. Here is the new line 11 `exec('wmic process list /format:csv', {maxBuffer: 2000*1024}, function (err, stdout, stderr) {`
+
+## node-spotify-webhelper
+You need to edit 2 lines here, line 15 the DEFAULT_PORT var, change the value to 4381. Next go to line 158 and change the `https://%s:%d%s` to `http://%s:%d%s`
+
+With them 2 files edited you should be good to go.
+
+# How to run
+Run `npm start` in console after the setup steps and boom! Rich Presence with your Spotify!
---
+++ index.js
@@ -0,0 +1,45 @@
+const DiscordRPC = require('discord-rpc'),
+	nodeSpotifyWebhelper = require('node-spotify-webhelper'),
+	spotify = new nodeSpotifyWebhelper.SpotifyWebHelper();
+
+const ClientId = "384286107036155904";
+
+const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+const startTimestamp = new Date();
+
+let startCount = -1;
+let guilds = 0;
+
+async function updateActivity() {
+	if (!rpc)
+    	return;
+    
+    spotify.getStatus(function(err, res) {
+    	if(err) {
+    		return console.error(err);
+    	}
+    	if(res.track.track_resource && res.track.track_resource.name){
+	    	rpc.setActivity({
+				details: `Currently playing ${res.track.track_resource.name}`,
+				state: `By ${res.track.artist_resource.name}`,
+				startTimestamp,
+				largeImageKey: 'spotify',
+				instance: false,
+			});
+			console.log(`[${new Date().toLocaleTimeString()}] Updated RPC rich presence - ${res.track.track_resource.name}`)
+		}
+    })
+}
+
+rpc.on('ready', () => {
+	console.log(`Starting with clentId ${ClientId}`);
+
+	updateActivity();
+
+	// activity can only be set every 15 seconds
+	setInterval(() => {
+		updateActivity();
+	}, 15e3);
+});
+
+rpc.login(ClientId).catch(console.error);
