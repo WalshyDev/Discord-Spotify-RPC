0c82d4c5499d559f1ba5329334cc9c407673a4ef
Dynista <carloslubbers111@gmail.com>
2017-11-29 12:04:20 +0100

    Improve recognition of paused songs, added check for presence of json API file

--- index.js
+++ index.js
@@ -7,16 +7,30 @@ async function updateActivity() {
 	if (!rpc)
     	return;
 
-		var fs = require('fs');
-    var obj = JSON.parse(fs.readFileSync(`${process.env.APPDATA}\\Google Play Music Desktop Player\\json_store\\playback.json`, 'utf8'));
+	var fs = require('fs');
+	var filepath = `${process.env.APPDATA}\\Google Play Music Desktop Player\\json_store\\playback.json`;
+	if(!fs.existsSync(filepath)) {
+		console.log("Can't find %APPDATA%\\Google Play Music Desktop Player\\json_store\\playback.json, do you " +
+		"have Google Play Music Desktop Player installed with the JSON API enabled?");
+		return;
+	}
+    var obj = JSON.parse(fs.readFileSync(filepath, 'utf8'));
     console.log(obj["song"]["title"]);
-
+	
+	if(obj["playing"] == true) {
 		rpc.setActivity({
-		details: `Playing ${obj["song"]["title"]}`,
-		state: `By ${obj["song"]["artist"]}`,
-		largeImageKey: 'gpm',
-		instance: false,
-	});
+			details: `Playing ${obj["song"]["title"]}`,
+			state: `By ${obj["song"]["artist"]}`,
+			largeImageKey: 'gpm',
+			instance: false,
+		});
+	} else {
+		rpc.setActivity({
+			details: `Nothing`,
+			largeImageKey: 'gpm',
+			instance: false,
+		});
+	}
 }
 
 rpc.on('ready', () => {
